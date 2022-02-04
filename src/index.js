6fea8ae9470243c9c5ddd46d967797e43c5210e3
hideki1516 <89428750+hideki1516@users.noreply.github.com>
2022-02-04 14:37:07 +0900

    Section-4 #20

--- src/index.js
+++ src/index.js
@@ -13,3 +13,37 @@ const val5 = ["dog", "cat"];
 val5[0] = "bird";
 val5.push("monkey");
 console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+
+const name = "じゃけぇ";
+const age = 28;
+const message1 = `私の名前は${name}です。年齢は${age}歳です。`;
+console.log(message1);
+
+/**
+ *  アロー関数
+ */
+
+// 従来の関数
+function func1(str) {
+  return str;
+}
+
+const func1 = function (str) {
+  return str;
+};
+
+console.log(func1("func 1です"));
+
+// アロー関数
+const func2 = (str) => str;
+// retunが1行の場合は{}省略可
+
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => num1 + num2;
+
+console.log(func3(10, 20));
